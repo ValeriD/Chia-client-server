3f47aaedbd5084c6970ad3fb1dc64d6609988b9b
Valeri <valer.dobrev10@gmail.com>
2021-09-24 14:54:54 +0300

    Updated the database uri

--- src/connections/db_connection.ts
+++ src/connections/db_connection.ts
@@ -2,9 +2,13 @@ import Logger from "jet-logger";
 import mongoose from "mongoose";
 import { DB } from "../config/config"
 
+
+const uri = (DB.user === "")? `mongodb://${DB.host}:${DB.port}/${DB.name}`:        `mongodb://${DB.user}:${DB.pass}@${DB.host}:${DB.port}/${DB.name}?authSource=admin`;
+
+
 const connectToDatabase = () =>{
     mongoose.connect(
-        `mongodb://${DB.user}:${DB.pass}@${DB.host}:${DB.port}/${DB.name}?authSource=admin`,
+        uri,
         {
             useNewUrlParser:true,
             useUnifiedTopology:true,
